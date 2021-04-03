9b32b6b9c60bcb127792302513f0347067a52bec
Nuno <komen>
2021-04-03 08:36:24 +0000

    Ubuntu update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,4 +1,4 @@
-<?php
+u<?php
 
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
@@ -7,10 +7,22 @@ use Facebook\WebDriver\Chrome\ChromeDriverService;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\WebDriverBy;
 
-require_once('C:\Users\nunom\Documents\GitHub\twitch-scraper\vendor\autoload.php');
-putenv('WEBDRIVER_CHROME_DRIVER=C:\Users\nunom\Documents\GitHub\twitch-scraper\tests\chromedriver.exe');
+require_once(__DIR__.'/../vendor/autoload.php');
+putenv('WEBDRIVER_CHROME_DRIVER=chromedriver');
 
-$driver = ChromeDriver::start();
+
+$options = new ChromeOptions();
+$options->addArguments(array(    
+    '--window-size=1280x800',
+    '--no-sandbox'
+));
+$capabilities = DesiredCapabilities::chrome();
+$capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
+$driver = ChromeDriver::start($capabilities);
+$host = 'https://twitch.tv';
+
+//$driver = RemoteWebDriver::create($host , $capabilities ,5000);
+
 // Go to URL
 $streamer = 'xqcow';
 $driver->get('https://www.twitch.tv/'.$streamer);
