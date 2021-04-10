a18435c19b92102669dcefd07c472388b079df8a
komen205 <59452877+komen205@users.noreply.github.com>
2021-04-10 13:56:17 +0100

    Update DefaultController.php

--- tests/DefaultController.php
+++ tests/DefaultController.php
@@ -3,7 +3,7 @@
 
 namespace App\Command\Twitch;
 
-require_once('C:\Users\nunom\Documents\GitHub\twitch-scraper\vendor\autoload.php');
+require_once(__DIR__.'\..\vendor\autoload.php');
 
 
 use GhostZero\Tmi\Client;
