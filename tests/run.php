df1db7238b625bcd8d4ca60a89d7bef175ec8eba
komen205 <59452877+komen205@users.noreply.github.com>
2021-04-11 12:25:26 +0100

    Update run.php

--- tests/run.php
+++ tests/run.php
@@ -22,7 +22,7 @@ for ($i = 0; $i <= count($streamers)-1; ++$i) {
        // pcntl_wait($status); //Protect against Zombie children
     } else {
         $GLOBALS['streamer'] = ($streamers[$i]->streamer);
-        echo($GLOBALS['streamer']);
+        echo($GLOBALS['streamer'].'\n');
         function check()
         {
             $url = 'https://api.twitch.tv/helix/streams/?user_login=' . $GLOBALS['streamer'];
