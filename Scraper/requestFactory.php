f1f521192fbb21f35820319f168b31980e2e0455
komen205 <59452877+komen205@users.noreply.github.com>
2021-04-24 19:01:36 +0100

    Update requestFactory.php

--- Scraper/requestFactory.php
+++ Scraper/requestFactory.php
@@ -43,7 +43,7 @@ class Request
             $this->requestUrl = 'http://localhost:8000/api/create/sub';
             $this->setFields();
             $this->request();
-            echo 'sub';
+            dump('sub');
             return;
         }
         if ($this->requestUrl == 'checkTwitchOnline') {
