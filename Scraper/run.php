c8872dc334016eb55a19d5f9e2f78c9a65c7d15f
komen205 <59452877+komen205@users.noreply.github.com>
2021-04-24 14:48:18 +0100

    Revert "Update run.php"
    This reverts commit ebbdc22f296d25eb3301d97d6af2410f75930ea1.

--- Scraper/run.php
+++ Scraper/run.php
@@ -10,7 +10,8 @@ use GhostZero\Tmi\Events\Twitch\AnonSubMysteryGiftEvent;
 use GhostZero\Tmi\Events\Twitch\ResubEvent;
 use GhostZero\Tmi\Events\Twitch\SubGiftEvent;
 use GhostZero\Tmi\Events\Twitch\SubMysteryGiftEvent;
-include('requestFactory.php');
+use RequestFactory;
+
 include('getStreamers.php');
 for ($i = 0; $i <= count($streamers) - 1; ++$i) {
 
